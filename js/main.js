b576b7fb86d920590ede8249a5e15b03a5cd9a72
MlgEpicCar <115513939+MlgEpicCar@users.noreply.github.com>
2024-10-23 13:39:57 -0400

    coded

---
+++ js/main.js
@@ -0,0 +1,41 @@
+document.getElementById("h1").textContent = `Goated Characters`;
+
+document.getElementById("h3").textContent = `Search Here:`;
+
+document.addEventListener("DOMContentLoaded", function() {
+    const inputField = document.getElementById("txt-predict");
+    const resultImage = document.getElementById("img-predict");
+
+    const images = [
+        "denji",
+        "makoto",
+        "shoko",
+        "shoya",
+        "yuri",
+        "yuta",
+        "aki",
+        "power",
+        "gojo",
+        "toji",
+        "sukuna",
+        "megumi",
+        "mlgepiccar"
+    ];
+    const notFoundImage = "not-found";
+
+    inputField.addEventListener("keyup", function() {
+        const inputValue = inputField.value.trim().toLowerCase();
+        const matchedImage = images.find(image => image.toLowerCase().startsWith(inputValue));
+        
+        if (inputValue === "") {
+            resultImage.src = "/images/" + notFoundImage + ".png";
+            return; 
+        }
+
+        if (matchedImage) {
+            resultImage.src = "/images/" + matchedImage + ".png";
+        } else {
+            resultImage.src = "/images/" + notFoundImage + ".png";
+        }
+    });
+});
